89b7fdc5a80c71c3d8e9cc60e9e4735a88854835
Marco Upia <markremembers@gmail.com>
2023-10-31 09:19:44 -0400

    Created 3 different function

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42
+    } else {
+        return 42 - blocks 
+    };
+};
+
+function distanceFromHqInFeet(blocks) {
+   return distanceFromHqInBlocks(blocks) * 264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+    let sum = 0;
+    if (start > destination) {
+        sum = start - destination
+    } else {
+        sum = destination - start 
+    };
+     return sum * 264;
+};
+
+function calculatesFarePrice(start, destination) {
+    const totalDistance = distanceTravelledInFeet(start, destination)
+    if (totalDistance <= 400) {
+        return 0;
+    } else if (totalDistance > 400 && totalDistance < 2000) {
+        return 0.02 * (totalDistance - 400);
+    } else if (totalDistance > 2000 && totalDistance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+};
+
+
